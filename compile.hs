2680f602d853cf76223c8d7d5a2fd4b02d5bf6d5
Andrew D'Angelo <dangeloandrew@outlook.com>
2017-02-20 22:32:56 -0600

    More basic instructions

--- compile.hs
+++ compile.hs
@@ -1,37 +1,29 @@
-import Data.Int
-import Data.Word
-import Data.Bits
-import Data.Monoid
-import Data.Typeable
-import Data.Binary.Put 
--- import Data.ByteString.Lazy.Char8
-
+-- Parser
 import Language.Scheme.Parser
 import Language.Scheme.Types
 
+-- Data types
+import Data.Int
+import Data.Word
+
+-- Machine state
+import Control.Monad.Trans.State
 import Data.HashMap.Strict as H (HashMap, empty, fromList, insert, lookup, union)
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-import Data.ByteString.Internal as BS (c2w, w2c)
-
-import Control.Exception
-import Control.Monad (when)
-import Control.Monad.Trans.Writer
-import Control.Monad.Trans.State
-import Control.Monad.Except
-
+-- REPL
 import System.IO
 
 -- Instructions and operands
+
+data Reg = R0 | R1 | R2 | LR | SP
+  deriving (Eq, Show)
 
 data Op = IntOp Int32
         | RegOp Reg
         | AddrOp Word32
         | RegOffsOp Reg Int
-  deriving (Eq, Show)
-
-data Reg = R0 | R1 | R2 | LR | SP
   deriving (Eq, Show)
 
 data Inst = BxInst Op
@@ -64,9 +56,12 @@ push (RegOp rd) = modify $ \s -> s {
   _stack = _stack s - 4}
 
 pop :: Op -> ARM ()
-pop (RegOp rd) = modify $ \s -> s {
-  _insts = ("ldr " ++ show rd ++ ", [sp, " ++ show (_stack s + 4) ++ "]"):(_insts s),
-  _stack = _stack s + 4}
+pop (RegOp rd) = modify $ \s ->
+  case _stack s of
+    0 -> s { _insts = "popped too much!":(_insts s) }
+    _ -> s {
+      _insts = ("ldr " ++ show rd ++ ", [sp, " ++ show (_stack s + 4) ++ "]"):(_insts s),
+      _stack = _stack s + 4}
 
 -- Operands
 
@@ -88,14 +83,39 @@ sp = RegOp SP
 -- Instructions
 
 bx :: Op -> ARM ()
-bx (RegOp rd) = do
+bx (RegOp rd) =
   emit $ "bx " ++ show rd
 
-add :: Op -> Op -> Op -> ARM ()
-add (RegOp rd) (RegOp rn) (RegOp op2) = do
-  emit $ "add " ++ show rd ++ ", " ++ show rn ++ ", " ++ show op2
-add (RegOp rd) (RegOp rn) (IntOp op2) = do
-  emit $ "add " ++ show rd ++ ", " ++ show rn ++ ", #" ++ show op2
+-- http://www.peter-cockerell.net/aalp/html/ch-3.html
+-- <op1>{cond}{S}{P}  <dest>,<lhs>,<rhs>
+g1o1 :: String -> Op -> Op -> Op -> ARM()
+g1o1 inst (RegOp rd) (RegOp rn) boxedop2 =
+  emit $ inst ++ " " ++ show rd ++ ", " ++ show rn ++ ", " ++ op2
+  where op2 = case boxedop2 of
+          (RegOp rop2) -> show rop2
+          (IntOp iop2) -> "#" ++ show iop2
+add = g1o1 "add"
+adc = g1o1 "adc"
+and = g1o1 "and"
+bic = g1o1 "bic"
+eor = g1o1 "eor"
+orr = g1o1 "orr"
+rsb = g1o1 "rsb"
+rsc = g1o1 "rsc"
+sbc = g1o1 "sbc"
+sub = g1o1 "sub"
+
+-- <op3>{cond}{S}{P}  <lhs>,<rhs>
+g1o3 :: String -> Op -> Op -> ARM()
+g1o3 inst (RegOp rn) boxedop2 =
+  emit $ inst ++ " " ++ show rn ++ ", " ++ op2
+  where op2 = case boxedop2 of
+          (RegOp rop2) -> show rop2
+          (IntOp iop2) -> "#" ++ show iop2
+teq = g1o3 "teq"
+tst = g1o3 "tst"
+cmn = g1o3 "cmn"
+cmp = g1o3 "cmp"
 
 -- Pseudoinstructions
 
@@ -103,20 +123,21 @@ comment :: String -> ARM()
 comment c = emit $ "@" ++ c
 
 move :: Op -> Op -> ARM ()
-move (RegOp rd) (RegOp op2) = do
+move (RegOp rd) (RegOp op2) =
   emit $ "mov " ++ show rd ++ ", " ++ show op2
-move (RegOp rd) (IntOp op2) = do
+move (RegOp rd) (IntOp op2) =
   emit $ "ldr " ++ show rd ++ ", =" ++ show op2
-move (RegOp rd) (RegOffsOp rn ro)
-  | ro == 0 = emit $ "ldr " ++ show rd ++ ", [" ++ show rn ++ "]"
-  | otherwise = emit $ "ldr " ++ show rd ++ ", [" ++ show rn ++ ", #" ++ show ro ++"]"
-move (RegOffsOp rn ro) (RegOp rd)
-  | ro == 0 = emit $ "str " ++ show rd ++ ", [" ++ show rn ++ "]"
-  | otherwise = emit $ "str " ++ show rd ++ ", [" ++ show rn ++ ", #" ++ show ro ++"]"
+move (RegOp rd) (RegOffsOp rn ro) =
+  emit $ "ldr " ++ show rd ++ ", [" ++ show rn ++ ", #" ++ show ro ++ "]"
+move (RegOffsOp rn ro) (RegOp rd) =
+  emit $ "str " ++ show rd ++ ", [" ++ show rn ++ ", #" ++ show ro ++ "]"
 
 -- Primitives
 
 primitiveOps = H.fromList [
+  ("add1", do
+      pop r0
+      add r0 r0 (IntOp 1)),
   ("+", do
       pop r0
       pop r1
@@ -130,7 +151,7 @@ compile (Number i) = move r0 (IntOp w)
 compile (Atom a) =
   case H.lookup a primitiveOps of
     Just inst -> inst
-    _ -> comment $ "atom '" ++ a ++ "' not found!"
+    _ -> emit $ "atom '" ++ a ++ "' not found!"
 compile (List l) =
   stackup $ reverse l
   -- for term in reverse tail l: compile l, push to stack, end for. pop into regs
@@ -140,14 +161,14 @@ compile (List l) =
           stackup es
         stackup [] = pop r0
 
--- clean slate
+-- initial state
 start = Program { _insts = [], _stack = 0, _cleanregs = Set.empty }
 -- create state transformations from source program
 transforms str = case readExpr str of
   (Right e) -> do
     compile e
     bx lr
-  _ -> comment "parse error"
+  _ -> emit $ "parse error: " ++ str
 
 -- REPL
 
